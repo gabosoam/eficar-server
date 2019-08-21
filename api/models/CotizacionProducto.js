c7869ae65cf07165a976290f4faad939eed0c0ee
gabosoam <gabrielsalazar@outlook.com>
2019-08-20 22:13:59 -0500

    first commit

---
+++ api/models/CotizacionProducto.js
@@ -0,0 +1,17 @@
+module.exports = {
+  attributes: {
+    cotizacion: {
+      model: 'cotizacion'
+    },
+    variante: {
+      model: 'tipoProducto'
+    },
+    porsentaje_iva: { type: 'number' },
+    cantidad: { type: 'number' },
+    precio: { type: 'number' },
+    tiempo_estandar: { type: 'number' },
+    porcentaje_descuento: { type: 'number' },
+    descripcion: { type: 'string' },
+    base_cero: { type: 'number' },
+  },
+}
